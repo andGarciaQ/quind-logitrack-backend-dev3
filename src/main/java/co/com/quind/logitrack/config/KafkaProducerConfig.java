75474f4e683b16919ac901984299de074555b329
andGarciaQ <andres.garcia@quind.io>
2025-12-05 19:16:39 -0500

    chore: union de configuraciones

--- src/main/java/co/com/quind/logitrack/config/KafkaProducerConfig.java
+++ src/main/java/co/com/quind/logitrack/config/KafkaProducerConfig.java
@@ -1,4 +1,4 @@
-package co.com.quind.logitrack.infrastructure.drivenadapters.kafkaproducer.config;
+package co.com.quind.logitrack.config;
 
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
